e2486254b42daeeb02a9c40c053133f8eed215c5
includeRubiks <fremonade@gmail.com>
2026-03-18 15:46:26 -0400

    Chagned associate instructions

--- src/BecomeAnAssociate.tsx
+++ src/BecomeAnAssociate.tsx
@@ -11,7 +11,7 @@ function BecomeAnAssociate(){
             <h2>Here's how:</h2>
             <h3>1. Email me at fremonade@gmail.com</h3>
             <h3>2. I'll review your printer and tell you if you are eligible to print for us</h3>
-            <h3>3. If you get in, you will be given files to print, and you will be given the material cost plus a dollar to print them. Give them to us, and we will sell them.</h3>
+            <h3>3. Congratulations, you are now an associate!</h3>
             <hr></hr>
         </div>
     )
